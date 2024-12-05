d5cfb05ad12d2a1679b29fa8b85c29bd2d729c9d
Laércio <laercio.melo@uni9.edu.br>
2024-12-05 02:56:53 -0300

    alterações com produtoPage

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,33 @@
 /// <reference types= "cypress" />
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
+
+
 describe('Funcionalidade: Produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos')
+      produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.first()
-           // .last()
-           // .eq(2)
-            .contains('Apollo Running Short')
-            .click()    
+        produtosPage.buscarProdutoLista('Ajax Full-Zip Sweatshirt')    
             cy.get('#tbay-main-content').should('exist')
 
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Aether Gym Pant'
+        produtosPage.buscarProduto(produto) 
+        cy.get('.product_title').should('contain', produto)
+        
+    });
+
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adcionar produto ao carrinho', () => {
+        
     });
 });
