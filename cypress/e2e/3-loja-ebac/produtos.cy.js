304ba189afe659dca06bab8b45a4fb6a3c2668ed
Laércio <laercio.melo@uni9.edu.br>
2024-12-07 02:06:12 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -16,7 +16,7 @@ describe('Funcionalidade: Produtos', () => {
 
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Aether Gym Pant'
         produtosPage.buscarProduto(produto) 
         cy.get('.product_title').should('contain', produto)
@@ -24,10 +24,32 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain', 'Aether Gym Pant')
+
     });
 
     it('Deve adcionar produto ao carrinho', () => {
+        let qtd = 4
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'Red', qtd)
+
+        cy.get('.woocommerce-message').should('contain',qtd + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+        
+    });
+
+
+    it.only('Deve adcionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados =>  {
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho, 
+                dados[0].cor, 
+                dados[0].quantidade)
+
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+        })
         
     });
 });
